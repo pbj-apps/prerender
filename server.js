d650f155ef8a1236e56eece58ba40922edd23b72
Vikalp Jain <mevikalp@gmail.com>
2020-07-05 19:38:06 +0530

    feat: fix prerendering header

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var prerender = require('./lib');
 
 var server = prerender();
 
-server.use(prerender.sendPrerenderHeader());
+// server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
