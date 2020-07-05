480ef340401330febce6cf37b92a8f83a5648a90
Vikalp Jain <mevikalp@gmail.com>
2020-07-05 19:50:54 +0530

    feat: add s3 cache

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
-
+var s3Cache = require('prerender-aws-s3-cache');
 var server = prerender();
 
 // server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
+server.use(s3Cache);
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
